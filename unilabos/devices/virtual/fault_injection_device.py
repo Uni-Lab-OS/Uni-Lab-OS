f58b502ac59f01c286794544e1b3dc62e37e560a
lixinyu1011 <61094742+lixinyu1011@users.noreply.github.com>
2026-06-05 12:00:07 +0800

    Update fault_injection_device.py

--- unilabos/devices/virtual/fault_injection_device.py
+++ unilabos/devices/virtual/fault_injection_device.py
@@ -218,3 +218,5 @@ class FaultInjectionDevice:
         raise EmergencyStopError(
             message="急停按钮已触发 (模拟-同步)",
         )
+
+# unilab --graph unilabos/test/experiments/fault_injection.json --config unilabos/test/experiments/fault_injection_config.py --ak a3d111bb-571a-4548-aa5d-c58ccca64466 --sk c2450c73-e84c-4319-b25f-b5cc4d575e7e --upload_registry --addr http://127.0.0.1:48197/api/v1
